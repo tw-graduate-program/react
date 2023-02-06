210e01575684961d778902d1148a614104d1226d
LYabstract <710116572@qq.com>
2023-02-06 13:40:33 +0800

    feat: render task name

---
+++ cypress/integration/task_list.text.js
@@ -0,0 +1,27 @@
+describe('task list', function () {
+    it('should render all task list with name', function () {
+        // Given
+        const mockTasks = [
+            {
+                "id": 1,
+                "name": "task 01",
+                "completed": false
+            },
+            {
+                "id": 2,
+                "name": "task 02",
+                "completed": true
+            }
+        ];
+        cy.intercept('GET', Cypress.env('API'), {
+            statusCode: 200,
+            body: mockTasks,
+        });
+        // When
+        cy.visit('/');
+        // Then
+        cy.get('li').should('have.length', 2);
+        cy.get('li').eq(0).contains(mockTakes[0].name);
+        cy.get('li').eq(1).contains(mockTakes[1].name);
+    });
+})
--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
 
 function App() {
-  return <div className="App">Hello React</div>;
+  const [tasks, setTasks] = useState([]);
+  useEffect(() => {
+    axios
+        .get('http://localhost:8080/tasks')
+        .then((response) => response.data)
+        .then((data) => setTasks(data));
+  },[]);
+
+  return (
+    <div className="App">
+      {tasks.map(task => <li key={task.key}>{task.name}</li>)}
+    </div>
+  );
 }
 
 export default App;
